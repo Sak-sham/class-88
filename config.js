af9bd25898d750aaaaeebf3d7561ca9cb467a73b
Saksham <67902670+Sak-sham@users.noreply.github.com>
2023-06-11 12:25:21 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBXiMWfTcpmY8uNfcU4MVyeA-vS7Pk9l7w",
+  authDomain: "class-85--storytellingapp.firebaseapp.com",
+  databaseURL: "https://class-85--storytellingapp-default-rtdb.firebaseio.com",
+  projectId: "class-85--storytellingapp",
+  storageBucket: "class-85--storytellingapp.appspot.com",
+  messagingSenderId: "917646044072",
+  appId: "1:917646044072:web:f0c5f90063480099d64b9d"
+};
